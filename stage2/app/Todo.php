7385d762146de1cd6591d2dc0a6c16550f5dedf6
VChernomor <chernomor.v1@ukr.net>
2020-02-11 15:39:46 +0200

    api v1, saving to json completed

--- stage2/app/Todo.php
+++ stage2/app/Todo.php
@@ -1,6 +1,10 @@
 <?php
 
-
+/**
+ * Class Todo
+ * A class can create, delete, edit, return records.
+ * And also stores them.
+ */
 class Todo
 {
     private $config;
@@ -10,6 +14,10 @@ class Todo
         $this->config = include 'config.php';
     }
 
+    /**
+     * Returns a list of all records.
+     * @return array List of entries.
+     */
     public function getItems(){
         if(!file_exists($this->config['data']['todo'])){
             $this->write();
@@ -21,6 +29,13 @@ class Todo
         return [];
     }
 
+    /**
+     * Modifies an existing entry by its id.
+     * If the entry is not found, displays an error.
+     * @param $id int identifier
+     * @param $text string Post text
+     * @param $checked bool status
+     */
     public function change($id, $text, $checked){
         $data = $this->getItems();
         foreach ($data as $item) {
@@ -35,6 +50,10 @@ class Todo
         $this->showError('Record with such id not found');
     }
 
+    /**
+     * Deletes an item by its identifier.
+     * @param $id int Identifier
+     */
     public function delete($id){
         $data = $this->getItems();
         foreach ($data as $item) {
@@ -48,6 +67,10 @@ class Todo
         $this->showError('Record with such id not found');
     }
 
+    /**
+     * Reads all items. Recognizes the identifier of the last item and creates a new item.
+     * @param $text string New Entry Text
+     */
     public function add($text){
         $data = $this->getItems();
         $id = 0;
@@ -62,6 +85,10 @@ class Todo
         $this->write($data);
     }
 
+    /**
+     * Writes all entries to a file.
+     * @param null $date
+     */
     private function write($date = null){
         $file = fopen($this->config['data']['todo'], 'w');
         if($date != null){
@@ -70,6 +97,10 @@ class Todo
         fclose($file);
     }
 
+    /**
+     * It displays an error message and terminates the script.
+     * @param $message string error message
+     */
     private function showError($message){
         header('HTTP/1.0 400 Bad Request');
         echo json_encode(['error' => $message]);
