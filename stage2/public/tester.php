4d000be1bd94ea65f9d9eb82348b65a6730447c2
VChernomorets <voldmikser@gmail.com>
2020-02-18 08:50:13 +0200

    cookie

---
+++ stage2/public/tester.php
@@ -0,0 +1,71 @@
+<?php
+
+$links = [
+    'v1' => [
+        'addItem' => 'http://localhost/api/v1/addItem.php',
+        'changeItem' => 'http://localhost/api/v1/changeItem.php',
+        'deleteItem' => 'http://localhost/api/v1/deleteItem.php',
+        'getItems' => 'http://localhost/api/v1/getItems.php',
+        'login' => 'http://localhost/api/v1/login.php',
+        'logout' => 'http://localhost/api/v1/logout.php',
+        'register' => 'http://localhost/api/v1/register.php'
+    ]
+];
+
+//checkLogout($links['v1']['logout']);
+checkGetItems($links['v1']['getItems']);
+//checkLogin($links['v1']['login']);
+
+//checkGetItems($links['v1']['getItems']);
+
+function checkAddItem(){
+
+}
+
+function checkGetItems($link){
+    query($link);
+}
+
+function checkLogin($link){
+    $user = [
+        'login' => 'Dima',
+        'pass' => 'vasyaPass'
+    ];
+    query($link, json_encode($user));
+}
+
+function checkLogout($link){
+    query($link);
+}
+
+function query($link, $data = ''){
+    $cookieFileName = 'cookie.txt';
+    //checkCookieFile($cookieFileName);
+    $query = curl_init();
+    curl_setopt($query, CURLOPT_URL, $link);
+    if($data !== '') {
+        curl_setopt($query, CURLOPT_HTTPHEADER, [
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($data)
+        ]);
+        curl_setopt($query, CURLOPT_POSTFIELDS, $data);
+    }
+    curl_setopt($query, CURLOPT_HEADER, true);
+    curl_setopt($query, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($query, CURLOPT_COOKIEFILE, $cookieFileName);
+    curl_setopt($query, CURLOPT_COOKIEJAR,  $cookieFileName);
+    print_r(curl_exec($query));
+    curl_close($query);
+
+    //CURLOPT_FAILONERROR
+}
+
+function checkCookieFile($cookieFileName){
+    if(!file_exists($cookieFileName)){
+        file_put_contents($cookieFileName, '');
+    }
+    if(!is_writable($cookieFileName)){
+        header('500 Internal Server Error');
+        exit('no permissions to write to ' . $cookieFileName);
+    }
+}
